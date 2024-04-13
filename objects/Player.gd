ec660baa65a40c91f1e3f5b8f2f61de37928247d
Bigaston <git@bigaston.me>
2024-04-13 12:58:29 +0200

    Couloir infinit

---
+++ objects/Player.gd
@@ -0,0 +1,40 @@
+extends CharacterBody3D
+
+@export var speed: float = 5.0
+
+@export_category("Sensitivity")
+@export var vertical_sensitivity = 0.01
+@export var horizontal_sensitivity = 0.01
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+@onready var neck := $Neck
+@onready var camera := $Neck/Camera3D
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent):
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			neck.rotate_y(-event.relative.x * horizontal_sensitivity)
+			camera.rotate_x(-event.relative.y * vertical_sensitivity)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x
+		velocity.z = direction.z
+	else:
+		velocity.x = 0.0
+		velocity.z = 0.0
+		
+	velocity = velocity.normalized()
+	velocity *= speed
+
+	move_and_slide()
---
+++ scripts/scenes/TerrainContainer.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+@export var terrain_scene: PackedScene
+@export var player: CharacterBody3D
+
+var terrain_array: Array[Node3D]
+
+func spawn_couloir(nb_to_spawn: int):
+	for i in range(nb_to_spawn):
+		var terrain = terrain_scene.instantiate() as Node3D
+		
+		var pos_z = -10 if (terrain_array.size() == 0) else (terrain_array[terrain_array.size() - 1].position.z - 20)
+		
+		terrain.position.z = pos_z
+		
+		add_child(terrain)
+		terrain_array.push_back(terrain)
+		
+func dispawn_couloir(nb_to_dispawn: int):
+	for i in range(nb_to_dispawn):
+		var node = terrain_array.pop_front()
+		node.queue_free()
+
+func _ready():
+	spawn_couloir(2)
+
+func _process(delta: float) -> void:
+	if player.position.z <= terrain_array[terrain_array.size()-1].position.z + 5:
+		spawn_couloir(1)
+		dispawn_couloir(1)
