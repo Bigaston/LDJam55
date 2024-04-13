14c9c77ad31d2ccc291278a6ccb9453903ca7cfb
Bigaston <git@bigaston.me>
2024-04-13 18:22:11 +0200

    Début final spell code

--- objects/ui/FinalSpell.gd
+++ objects/ui/FinalSpell.gd
@@ -1,6 +1,7 @@
 extends Control
 
-func set_spell(spell: BasicSpell):
+func set_spell(spell: FinalSpell):
 	$SpellName.text = spell.name
-
+	
+	$LeftText.text = spell.left_page_text
 	$RightText.text = spell.right_page_text
