b53c5421c24138a38b1b56fdf499cf63a96342bc
Bigaston <git@bigaston.me>
2024-04-13 17:33:29 +0200

    Store the monster on the parts folder

--- objects/Monster.gd
+++ objects/Monster.gd
@@ -3,17 +3,21 @@ extends Node3D
 @export var monster_speed: float = 1
 @export var monster_linker: MonsterResourceLinker
 
+var parts: Dictionary = {}
+
 func _ready():
 	random_part(CustomTypes.BodyPart.HEAD, $Body/Head)
 	random_part(CustomTypes.BodyPart.ARM, $Body/Arms)
 	random_part(CustomTypes.BodyPart.TORSO, $Body/Torso)
 	random_part(CustomTypes.BodyPart.LEGS, $Body/Legs)
-
+	
 func _process(delta: float) -> void:
 	position.z -= monster_speed * delta
 
 func random_part(part: CustomTypes.BodyPart, sprite: Sprite3D):
 	var family = CustomTypes.MonsterFamily.values()[ randi()%CustomTypes.MonsterFamily.size() ]
+
+	parts[part] = family
 
 	var linker = monster_linker.get_family(family)
 	
