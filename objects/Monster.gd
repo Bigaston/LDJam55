335416f2712fc84ae581a883085626dbaa3aa76d
Bigaston <git@bigaston.me>
2024-04-14 00:13:28 +0200

    Cinématique d'intro

--- objects/Monster.gd
+++ objects/Monster.gd
@@ -2,6 +2,9 @@ extends Node3D
 
 @export var monster_speed: float = 1
 @export var monster_linker: MonsterResourceLinker
+@export var time_before_start: float = 3
+
+var is_going = false
 
 var parts: Dictionary = {}
 
@@ -12,7 +15,8 @@ func _ready():
 	random_part(CustomTypes.BodyPart.LEGS, $Body/Legs, $Audio/LegsAudio)
 	
 func _process(delta: float) -> void:
-	position.z -= monster_speed * delta
+	if is_going:
+		position.z -= monster_speed * delta
 
 func random_part(part: CustomTypes.BodyPart, sprite: Sprite3D, audio: AudioStreamPlayer3D):
 	var family = CustomTypes.MonsterFamily.values()[ randi()%CustomTypes.MonsterFamily.size() ]
@@ -24,3 +28,8 @@ func random_part(part: CustomTypes.BodyPart, sprite: Sprite3D, audio: AudioStrea
 	
 	sprite.texture = texture
 	audio.stream = linker.get_audio(part)
+
+func trigger_move():
+	get_tree().create_timer(time_before_start).timeout.connect(func():
+		is_going = true
+	)
