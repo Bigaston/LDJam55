7f8de9d15096323e9418db1d4135a59a35837773
Bigaston <git@bigaston.me>
2024-04-14 18:43:22 +0200

    Fix legs

--- objects/Monster.gd
+++ objects/Monster.gd
@@ -48,9 +48,12 @@ func set_body_part(part: CustomTypes.BodyPart, family: CustomTypes.MonsterFamily
 	
 	if part == CustomTypes.BodyPart.LEGS:
 		if leg != null:
+			leg.unequip()
 			leg.queue_free()
 			
 		leg = monster_linker.get_family(family).legs_object.instantiate()
+		
+		leg.equip()
 		
 		$Body.add_child(leg)
 	else:
@@ -66,6 +69,7 @@ func set_body_part(part: CustomTypes.BodyPart, family: CustomTypes.MonsterFamily
 func trigger_move():
 	get_tree().create_timer(time_before_start).timeout.connect(func():
 		is_going = true
+		leg.set_monster_walking(true)
 	)
 
 func use_basic_spell(spell: BasicSpell):
@@ -89,3 +93,4 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is Player:
 		body.kill_player()
 		is_going = false
+		leg.set_monster_walking(false)
---
+++ objects/monster_legs/harpie_leg.gd
@@ -0,0 +1,14 @@
+extends MonsterLegs
+
+func equip():
+	pass
+	#$AnimationPlayer.play("Walk")
+	
+func unequip():
+	$AnimationPlayer.stop()
+
+func set_monster_walking(walking: bool):
+	if walking:
+		$AnimationPlayer.play("Walk")
+	else:
+		$AnimationPlayer.stop()
